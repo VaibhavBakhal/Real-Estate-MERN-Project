4a07a9790adf7a3688c4383bccf41ffe30fba10c
VaibhavBakhal <vaibhavbakhal609@gmail.com>
2023-08-09 11:07:59 +0530

    website url added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       domain={import.meta.env.VITE_DOMAIN}
       clientId={import.meta.env.VITE_CLIENTID}
       authorizationParams={{
-        redirect_uri: import.meta.env.VITE_REDIRECT_URI,
+        redirect_uri: "https://real-estate-ef1fz2cum-vaibhavbakhal.vercel.app/",
       }}
       audience={import.meta.env.VITE_AUDIENCE}
       scope="openid profile email"
